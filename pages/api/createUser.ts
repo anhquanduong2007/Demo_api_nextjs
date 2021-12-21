751f12c0121751ad55ffea98fd6aab7ba2706f42
valaqua20 <dreamlyrics2210@gmail.com>
2021-12-21 17:30:40 +0700

    fix add user api

--- pages/api/createUser.ts
+++ pages/api/createUser.ts
@@ -8,16 +8,16 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  console.log(req.method);
   if (req.method === "POST") {
     const { id, name, username, email, address } = req.body;
     const newUser: User = {
-      id,
+      id: Number(id),
       name,
       username,
       email,
       address,
     };
+    data.push(newUser);
     res.status(200).json({ data });
   } else {
     res.status(405);
